e719aa618141ec8d99804df3f59cb79fc0ce3bd7
@Jeremyobrien <jjob1391@gmail.com>
2022-08-04 23:08:18 -0400

    Add spinning number feature

--- src/App.js
+++ src/App.js
@@ -1,26 +1,89 @@
-import { useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
+import Number from './Number';
 import './App.css';
 
 function App() {
   const [number, setNumber] = useState(0);
+  const [inRange, setInRange] = useState(true);
+
+  // useEffect( () => {
+  //   checkRange();
+  // }, [number])
+
+  const checkRange = () => {
+    if (number !== 0 || number > 1000){
+      if (!inRange) {
+        alert('bummer')
+      } else {
+        alert('winner')
+      }
+    }
+  }
+
+  // const number = useMemo( () => {
+  //   const value = number;
+  //   return value;
+  // }, [number]);
+
+  // useEffect( () => {
+  //     // if ( number > 1000) {
+  //     //   setInRange(false);
+  //     //   checkRange();
+  //     // }
+
+
+  // }, [ number, inRange ]);
+
+  // useEffect(() => {
+  //   if (!inRange) {
+  //     alert('you lose');
+  //     setInRange(true);
+  //     setNumber(0);
+  //   }
+  // }, [inRange])
 
   const getRandomNumber = (max, min) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
   const handleClick = () => {
-    setNumber(getRandomNumber(1, 75000));
+    let i = 0;
+    let timer;
+    while( i < 400){
+      const spinningNumbers = getNumbers();
+      for ( let i = 0; i < spinningNumbers.length; i++){
+        timer = setTimeout(() => setNumber(spinningNumbers[i]), 10);
+      }
+      i++;
+    } 
+    return () => clearTimeout(timer);
   }
 
-  return (
-    <div className="App">
-      <h1>The JavaScript Lottery</h1>
-      <h2>Click the button to draw a number</h2>
-      <button onClick={handleClick}>Let's Play</button>
-      <h3>Your Number: {number}</h3>
-      <p>* Any number in the 1 to 1000 range wins!</p>
-    </div>
-  );
+  const getNumbers = () => {
+    let numberArr = [];
+    let i = 0;
+    while(i < 50) {
+      const number = getRandomNumber(1, 75000);
+      numberArr.push(number);
+      i++;
+    }
+    return numberArr;
+  }
+
+
+  console.log(number);
+  // console.log(data);
+
+    return (
+      <div className="App">
+        <h1>The JavaScript Lottery</h1>
+        <h2>Click the button to draw a number</h2>
+        <button onClick={handleClick}>Let's Play</button> 
+        <h3>Your Number: {number}</h3>
+        {/* <Number number={number} inRange={inRange}/> */}
+        <p>* Any number in the 1 to 1000 range wins!</p>
+      </div>
+    );
 }
 
 export default App;
---
+++ src/Number.js
@@ -0,0 +1,12 @@
+import {useState} from 'react';
+
+const Number = (props) => {
+
+    return(
+        <>
+            <h3>Your Number: {props.number}</h3>
+        </>
+    );
+}
+
+export default Number;
